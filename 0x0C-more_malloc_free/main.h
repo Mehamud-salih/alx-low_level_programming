ed2b61444e46b9faa43043b2aa4c7f6ce0656e2c
Mehamud-salih <mahmudsalih96@gmail.com>
2022-04-09 00:33:23 +0300

    header file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,6 +7,7 @@ int *array_range(int min, int max);
  * @size: number of bytes
  * Return: pointer value
  */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void *_calloc(unsigned int nmemb, unsigned int size);
 /**
  * string_nconcat - concats strings
