42b04fa50f6fb8879112cafce3db2f68e914c5d0
Mehamud-salih <mahmudsalih96@gmail.com>
2022-04-13 22:05:10 +0300

    self opcodes printer

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,25 +10,25 @@
  */
 int main(int argc, char *argv[])
 {
- int b;
- char *mem = (char *) main;
+	int b;
+	char *mem = (char *) main;
 
- if (argc != 2)
- {
-  printf("Error\n");
-  exit(1);
- }
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
 
- b = atoi(argv[1]);
+	b = atoi(argv[1]);
 
- if (b < 0)
- {
-  printf("Error\n");
-  exit(2);
- }
+	if (b < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
 
- while (b--)
-  printf("%02x%c", *mem++ & 0xff, b ? ' ' : '\n');
+	while (b--)
+		printf("%02x%c", *mem++ & 0xff, b ? ' ' : '\n');
 
- return (0);
+	return (0);
 }
